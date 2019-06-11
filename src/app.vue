9516af5c1681129f40ba2f8e67adba6a48aa500b
wangbing <wangbin01@wondersgroup.com>
2019-06-11 09:34:50 +0800

    修改冲突

--- src/app.vue
+++ src/app.vue
@@ -4,31 +4,52 @@
   </div>
 </template>
 <script>
-  export default {
-    name: 'app',
-    data() {
-      return {}
+export default {
+  name: 'app',
+  data () {
+    return {}
     },
-    mounted() {
-      window.onload = function () {
-        window.scrollTo(0, 0)
+  mounted () {
+    window.onload = function () {
+      window.scrollTo(0, 0)
       };
-    },
-    watch: {
-      '$route.path'(newV, oldV) {
-        if (newV === oldV) return false;
-        const path = newV;
+  },
+  watch: {
+    '$route.path' (newV, oldV) {
+      if (newV === oldV) return false
+        const path = newV
         //配置无需登陆的页面数组
-        const pathArr = ['login', 'groupCommandPage', 'activate', 'userProtocol', 'guideArticle', 'activePage',
-          'activity_index',
-          'activity_ground', 'baili2_index', 'resetPassword', 'companyLogin', 'family_ground', 'vip_activate',
-          'vip_activate2', 'groupCommand', 'card_question', 'activity_BCM_success', 'activity_BCM_index',
-          'activity_activate', 'activity_activate2', 'activity_invitation', 'memberEquity'
-        ];
-        if (!localStorage.token && path != '/' && pathArr.filter(x => path.indexOf(x) > -1).length == 0) {
-          dooolyAPP.logOut(1);
+        const pathArr = [
+        'login',
+        'groupCommandPage',
+        'activate',
+        'userProtocol',
+        'guideArticle',
+        'activePage',
+        'activity_index',
+        'activity_ground',
+        'baili2_index',
+        'resetPassword',
+        'companyLogin',
+        'family_ground',
+        'vip_activate',
+        'vip_activate2',
+        'groupCommand',
+        'card_question',
+        'activity_BCM_success',
+        'activity_BCM_index',
+        'activity_activate',
+        'activity_activate2',
+        'activity_invitation',
+        'memberEquity'
+      ]
+        if (
+        !localStorage.token &&
+          path != '/' &&
+          pathArr.filter(x => path.indexOf(x) > -1).length == 0
+      ) {
+        dooolyAPP.logOut(1)
         }
-      }
     }
   }
 }
