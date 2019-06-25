d2578cf0e2332b7ab7cf8a0e3dda03976d8f3121
chenxin <506599987@qq.com>
2019-06-25 11:24:02 +0800

    大华登录优化

--- src/components/common/guideGoods.vue
+++ src/components/common/guideGoods.vue
@@ -101,3 +101,301 @@
 
   }
 </script>
+<style scoped>
+  .top {
+    height: 0.78rem;
+    background: #F7F7F7;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    font-size: 0.32rem;
+    color: #999999;
+    align-items: center;
+  }
+  .top-checked{
+    color: #FD5739;
+  }
+  .down-sort-price {
+    border-bottom: 4px solid #FD5739 !important;
+  }
+  .up-sort-price {
+    border-top: 4px solid #FD5739 !important;
+  }
+  .triangle-up {
+    display: inline-block;
+    width: 0;
+    height: 0;
+    overflow: hidden;
+    line-height: 0;
+    font-size: 0;
+    vertical-align: middle;
+    margin: -10px 0 0 0;
+    border-bottom: 4px solid #A2A2A2;
+    border-top: 0 none;
+    border-left: 4px solid transparent;
+    border-right: 4px solid transparent;
+    color: #FF3FFF;
+    filter: chroma(color=#FF3FFF);
+  }
+  .triangle-down {
+    display: inline-block;
+    width: 0;
+    height: 0;
+    overflow: hidden;
+    line-height: 0;
+    font-size: 0;
+    vertical-align: middle;
+    margin: 3px 0 0 -13px;
+    border-top: 4px solid #A2A2A2;
+    border-bottom: 0 none;
+    border-left: 4px solid transparent;
+    border-right: 4px solid transparent;
+    color: #FF3FFF;
+    filter: chroma(color=#FF3FFF);
+  }
+  .goods-wrapper {
+    height: 2.4rem;
+    padding: 0.4rem 0.3rem 0 0.3rem;
+    background: #fff;
+    box-sizing: border-box;
+  }
+  .goods-wrapper:last-child{
+    margin-bottom: 0.4rem;
+  }
+  .list-left {
+    height: 2rem;
+    width: 2rem;
+    background: url(../../assets/images/shoppingGuide/goods.png) no-repeat 0 0;
+    background-size:100% 100%;
+
+  }
+
+  .list-left img {
+    width: 100%;
+    height: 100%;
+  }
+
+  .list-right {
+    padding-left: 0.1rem;
+    height: 2rem;
+    width: 4.48rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+  }
+
+  .list-content-top {
+    height: 0.8rem;
+    width: 4.48rem;
+  }
+
+  .goods-name {
+    height: 0.8rem;
+    font-size: 0.32rem;
+    color: #333333;
+    line-height: 0.4rem;
+    overflow: hidden; 
+    text-overflow: ellipsis; 
+    display: -webkit-box; 
+    -webkit-line-clamp: 2; 
+    -webkit-box-orient: vertical; 
+  }
+
+  .goods-name span {
+    font-size: 0.24rem;
+    color: #CFA864;
+    border-radius: 1px;
+    margin-right: 0.02rem;
+    display: inline-block;
+    padding: 0 0.05rem;
+    text-align: center;
+    vertical-align: top;
+    position: relative;
+    margin-right: 0.1rem;
+    overflow: hidden;
+  }
+  .goods-name span i:nth-child(1),.buttom-left i:nth-child(1){
+    content: '';
+    position: absolute;
+    width: 200%;
+    height: 1px;
+    top: 0;
+    left:0;
+    border-bottom: 1px solid #CFA864;
+    -webkit-transform-origin: 0 0;
+    transform-origin: 0 0;
+    -webkit-transform: scale(.5,.5);
+    transform: scale(.5,.5);
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box
+  }
+  .goods-name span i:nth-child(2),.buttom-left i:nth-child(2){
+    content: '';
+    position: absolute;
+    width: 1px;
+    height: 200%;
+    top: 0;
+    right:0;
+    border-right: 1px solid #CFA864;
+    -webkit-transform-origin: 0 0;
+    transform-origin: 0 0;
+    -webkit-transform: scale(.5,.5);
+    transform: scale(.5,.5);
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box
+  }
+  .goods-name span i:nth-child(3),.buttom-left i:nth-child(3){
+    content: '';
+    position: absolute;
+    width: 200%;
+    height: 1px;
+    bottom: 0;
+    left:0;
+    border-bottom: 1px solid #CFA864;
+    -webkit-transform-origin: 0 0;
+    transform-origin: 0 0;
+    -webkit-transform: scale(.5,.5);
+    transform: scale(.5,.5);
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box
+  }
+  .goods-name span i:nth-child(4),.buttom-left i:nth-child(4){
+    content: '';
+    position: absolute;
+    width: 1px;
+    height: 200%;
+    top: 0;
+    left:0;
+    border-left: 1px solid #CFA864;
+    -webkit-transform-origin: 0 0;
+    transform-origin: 0 0;
+    -webkit-transform: scale(.5,.5);
+    transform: scale(.5,.5);
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box
+  }
+
+  .list-content-middle {
+    height: 0.4rem;
+    font-size: 0.32rem;
+    color: #CFA864;
+  }
+
+  .list-content-middle-color{
+    color:#FD5739;
+    display: inline-block;
+    line-height: 0.4rem;
+  }
+  .list-content-middle .fl{max-width: 1.9rem;overflow: hidden;text-overflow: ellipsis;}
+
+  .middle-right {font-size: 0;max-width:2.5rem;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;}
+
+  .doooly-label {
+    background: #FD5739;
+    border-radius: 1px;
+    font-size: 0.24rem;
+    color: #FFFFFF;
+    line-height: 0.4rem;
+    display: inline-block;
+    text-align: center;
+    vertical-align: top;
+    padding: 0 0.1rem;
+  }
+
+  .doooly-price {
+    font-size: 0.32rem;
+    color: #FD5739;
+    display: inline-block;
+    text-align: center;
+    letter-spacing: 0;
+    padding-left: 0.1rem;
+  }
+
+  .list-content-middle i {
+    font-style: normal;
+    margin-right: -3px;
+  }
+
+  .list-content-buttom {
+    font-size: 0.24rem;
+  }
+
+  .buttom-left {
+    float: left;
+    font-size: 0;
+    white-space: nowrap;
+    position: relative;
+    padding: 1px;
+  }
+  .plan {
+    color: #FFFFFF;
+    background: #CFA864;
+    border-radius: 1px 0 0 1px;
+    font-size: 0.24rem;
+    display: inline-block;
+    height: 0.34rem;
+    line-height: 0.34rem;
+    padding:0 0.05rem;
+    text-align: center;
+    box-sizing: border-box;
+  }
+
+  .point {
+    background: #FFFFFF;
+    border-radius: 0 1px 1px 0;
+    line-height: 0.34rem;
+    font-size: 0.24rem;
+    display: inline-block;
+    text-align: center;
+    box-sizing: border-box;
+    padding:0 0.05rem;
+    color: #CFA864;
+    position: relative;
+  }
+  .sales-right {
+    height: 0.34rem;
+    color: #999999;
+    font-size: 0.24rem;
+    float: right;
+  }
+  .loadContainer {
+    width: 100%;
+    font-size: 0.24rem;
+    text-align: center;
+    padding: 0.5rem 0;
+    background: #f5f5f5;
+    color: #999;
+    position: relative;
+
+  }
+
+  .loadText {
+    width: 2.1rem;
+    height: 1px;
+    background: #ececec;
+    position: absolute;
+    left: 0.3rem;
+    top: 50%;
+  }
+
+  .loadLine {
+    width: 2.1rem;
+    height: 1px;
+    background: #ececec;
+    position: absolute;
+    right: 0.3rem;
+    top: 50%;
+  }
+
+  .mint-loadmore-bottom {
+    width: 100%;
+    font-size: 0.26rem;
+    text-align: center;
+    padding: 0.1rem 0;
+  }
+
+  .mint-loadmore-bottom-img {
+    width: 1rem;
+  }
+</style>
