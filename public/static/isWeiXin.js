10aa16138b596d9010cf1fd8cfdf7ffc249d4afc
haoshan <1044917411@qq.com>
2019-08-05 21:33:12 +0800

    第三方地址参数大华

--- public/static/isWeiXin.js
+++ public/static/isWeiXin.js
@@ -214,10 +214,10 @@ var activityUrls = {
   Doooly: 'https://reach-life.com/pro_activity/activity/#/'
 }
 var otherPayUrls = {
-  baseUrlTest: 'https://admin.doooly.com/reachtest/thirdPartyPay/#/cardBuyPay/',
-  baseUrlMain: 'https://admin.doooly.com/reach_dist/thirdPartyPay/#/cardBuyPay/',
-  fabuButter: 'https://reach-life.com/pre_cashier/thirdPartyPay/#/cardBuyPay/',
-  Doooly: 'https://reach-life.com/pro_cashier/thirdPartyPay/#/cardBuyPay/'
+  baseUrlTest: 'https://admin.doooly.com/reachtest/thirdPartyPay/#/',
+  baseUrlMain: 'https://admin.doooly.com/reach_dist/thirdPartyPay/#/',
+  fabuButter: 'https://reach-life.com/pre_cashier/thirdPartyPay/#/',
+  Doooly: 'https://reach-life.com/pro_cashier/thirdPartyPay/#/'
 }
 // 微信首页跳转签名url
 if (/wiscowechat/.test(window.location.href)) {
