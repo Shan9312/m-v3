dbb0804a5d57b05f66e324ddca929a72b4673711
zhaoyang <18355403288@163.com>
2019-08-14 11:56:50 +0800

    test

--- src/components/doooly/v3/pages/product-detail/pick-up-goods/detail.js
+++ src/components/doooly/v3/pages/product-detail/pick-up-goods/detail.js
@@ -146,7 +146,8 @@ export default {
         giftBagId: this.giftBagId,
         orderType: this.cardBuyDetailList.selfProduct.productAttr
       };
-      if (this.activityName === 'jianhangTicketOther') return this.$router.push({ path: `/v3/constructOrderDetail/${this.$route.params.productId}/${this.activityName}` });
+      // if (this.activityName === 'jianhangTicketOther') return this.$router.push({ path: `/v3/constructOrderDetail/${this.$route.params.productId}/${this.activityName}` });
+      if (this.activityName === 'jianhangTicketOther') return dooolyAPP.gotoJumpVue(this.$router, `/v3/constructOrderDetail/${this.$route.params.productId}/${this.activityName}`);
       if (this.postData.productType == 1) {
         this.$router.push({ path: '/orderInfo_entity' });
       } else if (this.postData.productType === '11') {
