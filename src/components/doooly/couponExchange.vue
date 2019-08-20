60f75c47970a386107e539cba41f1411605c5b88
zhaoyang <18355403288@163.com>
2019-08-20 14:41:14 +0800

    update pro id

--- src/components/doooly/couponExchange.vue
+++ src/components/doooly/couponExchange.vue
@@ -81,7 +81,7 @@
         <div class="close" @click="cloe()"></div>
         <div class="title"><span>兑换礼券</span></div>
         <!-- pro couponId [1072, 1073, 1074] -->
-        <div v-if="couponDetail.couponId !== 1075 && couponDetail.couponId !== 111 && couponDetail.couponId !== 222" class="coupon">
+        <div v-if="couponDetail.couponId !== 1072 && couponDetail.couponId !== 1073 && couponDetail.couponId !== 1074" class="coupon">
           <span class="coupon_title">兑换码</span><span id="copyCode" class="detail">{{couponDetail.couponCode}}</span>
           <div class="bottom_btn">
             <div v-if="couponDetail.adBusiness.dealType == 0">
