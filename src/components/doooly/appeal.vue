8b19a06c8ccbd7203206a9bb9246d9fe4b1a3ef8
chenxin <506599987@qq.com>
2019-08-22 15:44:59 +0800

    修改安卓上传图片方法

--- src/components/doooly/appeal.vue
+++ src/components/doooly/appeal.vue
@@ -325,7 +325,7 @@
         },
         //app选择照片
         chooseImgAPP(){
-          if(this.$browserName == 'otherAPPAndroid'){
+          if(this.$browserName == 'otherAPPAndroid' || this.$browserName == 'Chrome WebView'){ 
             dooolyAPP.photographNative('APP_chooseImage');
           }
         },
